9e0af058db4f81e2bf2976d2c4ff557850ac30c1
Timofey Ivaykin <ivaykin@live.com>
2016-01-11 02:40:26 +1000

    First full deploy

---
+++ locales/vf_ru/definitions.php
@@ -0,0 +1,10 @@
+<?php
+
+ $LocaleInfo['vf_ru'] = array (
+  'Locale' => 'ru',
+  'Name' => 'Русский / Russian',
+  'Description' => 'Official Russian language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/ru/">here</a>.',
+  'Version' => '2015.12.18p2001',
+  'Author' => 'Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/ru/',
+);
